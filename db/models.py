1497ac04f859a3957a9cbcfa2246a16983cd7853
Enot777ogg <63520770+Enot777ogg@users.noreply.github.com>
2025-07-10 20:21:13 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
- from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
