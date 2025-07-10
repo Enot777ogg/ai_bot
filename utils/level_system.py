b5c68b04f7662c2b06a00e6af5d5c9d48b1012da
Enot777ogg <63520770+Enot777ogg@users.noreply.github.com>
2025-07-10 20:08:37 +0300

    Update level_system.py

--- utils/level_system.py
+++ utils/level_system.py
@@ -1,8 +1,8 @@
 def calculate_level(xp: int) -> int:
     level = 1
-    required = 1000  # на 1 уровень
+    required = 1000
     while xp >= required:
-        level += 1
         xp -= required
+        level += 1
         required = int(required * 2.5)
     return level
