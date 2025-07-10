136030f13dc69271d5ffc402ca71d42f8e6949e0
Enot777ogg <63520770+Enot777ogg@users.noreply.github.com>
2025-07-10 20:12:44 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from handlers.menu_handler import start, handle_menu_choice
+from handlers.text_handler import handle_text
+from db.session import init_db
+
+# Загрузи токен
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # добавь в .env или в Termux вручную
+init_db()
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_menu_choice))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_text))
+
+    print("Бот запущен.")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
